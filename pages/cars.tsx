e95edc6a266d10f79b51edcf3af294a09b63eebe
Your Name <your.email@example.com>
2025-06-23 06:23:15 +0300

    fix: corrected import in cars.tsx

--- pages/cars.tsx
+++ pages/cars.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
 import { useEffect, useState } from 'react'
-import { supabase } from '../utils/supabaseClient'
+import supabase from '../utils/supabaseClient'
 
 export default function Cars() {
   const [cars, setCars] = useState<any[]>([])
